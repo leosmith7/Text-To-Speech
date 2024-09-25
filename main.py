042ac7207e202f4ada5ec3a44a917cf5f897c32b
Leo Smith <ecomstoreleo@gmail.com>
2024-09-25 14:29:36 +0200

    fix API request params and add save audio

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import os
 
 API_KEY = os.getenv('API_KEY')
 
-
-
-
 class PDFToSpeechApp:
     def __init__(self, window):
         self.window = window
@@ -22,35 +19,36 @@ class PDFToSpeechApp:
         if file_path:
             text = self.extract_text_from_pdf(file_path)
             audio_data = self.text_to_speech(text)
-            self.save_audio(audio_data)
-            self.play_audio()
+            if audio_data:
+                self.save_audio(audio_data)
 
     def extract_text_from_pdf(self, file_path):
         combined_text = ''
         if file_path:
-            with open(f'{file_path}', 'rb') as file:
+            with open(file_path, 'rb') as file:
                 reader = PyPDF2.PdfReader(file)
                 for page in reader.pages:
-                    combined_text = combined_text + page.extract_text() + ' '
+                    combined_text += page.extract_text() + ' '
         return combined_text
 
     def text_to_speech(self, text):
-        api_params = {'key': API_KEY, 'src': text, 'hl' : 'en-us'}
-        response = requests.get(f'https://api.voicerss.org/?<parameters>')
+        response = requests.get(f'https://api.voicerss.org/?key={API_KEY}&src={text}&hl=en-us&c=WAV')
+        if response.status_code == 200 and response.content:
+            print(response.content)
+            return response.content
+
+        else:
+            print("Error:", response.status_code)
+            return None
 
     def save_audio(self, audio_data):
-        # TODO: Implement audio saving functionality
-        pass
-
-    def play_audio(self):
-        # TODO: Implement audio playback
-        pass
-
+        file_path = filedialog.asksaveasfilename(defaultextension=".wav", filetypes=[("WAV Files", "*.wav")])
+        if file_path:
+            with open(file_path, 'wb') as audio_file:
+                audio_file.write(audio_data)
+            print(f"Audio saved to {file_path}")
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = PDFToSpeechApp(root)
     root.mainloop()
-
-
-
